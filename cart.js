2a6ecc3a1d2dbe77c24a58916941fadd11d9a4ad
AJ Larson <aj.larson@devmounta.in>
2017-11-28 15:14:02 -0700

    sanitizing

--- cart.js
+++ cart.js
@@ -5,17 +5,14 @@ module.exports = {
   total: 0,
 
   addToCart: function( car ) {
-    this.cart.push( car );
-    this.total += car.price;
+
   },
 
   removeFromCart: function( index, price ) {
-    this.cart.splice( index, 1 );
-    this.total -= price;
+
   },
 
   checkout: function() {
-    this.cart = [];
-    this.total = 0;
+
   }
 };
